8d64984f6871bad21813f1eea9f83988cf62ac4d
shelrj13 <rjsshelton@gmail.com>
2017-04-25 08:03:25 -0500

    changed login js

--- app.js
+++ app.js
@@ -1,44 +1,53 @@
 (function(){
-	
- 	// Initialize Firebase
+  
+  // Initialize Firebase
 
- 	var config = {
-   		apiKey: "AIzaSyCUExeJi2L8KNBxVXuUJLOjwavJ57dEg-Y",
-    	authDomain: "compsci372.firebaseapp.com",
-    	databaseURL: "https://compsci372.firebaseio.com",
-    	projectId: "compsci372",
-    	storageBucket: "compsci372.appspot.com",
-    	messagingSenderId: "444915101658"
-  	};
+  var config = {
+      apiKey: "AIzaSyCUExeJi2L8KNBxVXuUJLOjwavJ57dEg-Y",
+      authDomain: "compsci372.firebaseapp.com",
+      databaseURL: "https://compsci372.firebaseio.com",
+      projectId: "compsci372",
+      storageBucket: "compsci372.appspot.com",
+      messagingSenderId: "444915101658"
+    };
   firebase.initializeApp(config);
 
   //get elements
-  const inputEmail = document.getElementById('inputEmail');
+  const inputUsername = document.getElementById('inputUsername');
   const inputPass = document.getElementById('inputPass');
   const login = document.getElementById('login');
 
+  //local storage
+   localStorage.setItem("UserName", null);
+
   //add login event
   login.addEventListener('click', e => {
-  	//get email and pass
-  	const email = inputEmail.value;
-  	const pass = inputPass.value;
-  	const auth = firebase.auth();
-  	//sign in
-  	const promise = auth.signInWithEmailAndPassword(email,pass);
-  	promise.catch(e => console.log(e.message));
 
-  	//add a realtime listener
-  	firebase.auth().onAuthStateChanged(firebaseUser => {
-  		if(firebaseUser){
-  			console.log(firebaseUser);
-  			//document.location.href = 'index.html';
-  		} else {
-  			console.log('not logged in');
-  		}
-  	});
-  	
+    //get email and pass
+    const user = inputUsername.value;
+    const pass = inputPass.value;
+
+    var firebaseRef = firebase.database().ref("Employees");
+
+    firebaseRef.once('value', function(snapshot) {
+      if (snapshot.hasChild(user))
+        {
+          localStorage.setItem("UserName", user);
+          
+        }
+        else
+        {
+          localStorage.setItem("UserName", null);
+          
+        }
+
+        document.getElementById("result").innerHTML = localStorage.getItem("UserName");
+    });
+
+
+    
+    
   });
-
 
 
 }());
